7bc3a4415bc10f60a061486e14c28cedcd1ff6d3
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 19:47:52 -0400

    Added standardized test package and updated generator version

--- conanfile.py
+++ conanfile.py
@@ -13,7 +13,7 @@ class BoostFilesystemConan(ConanFile):
     lib_short_names = ["filesystem"]
     options = {"shared": [True, False]}
     default_options = "shared=False"
-    build_requires = "Boost.Generator/0.0.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.64.0@bincrafters/testing"
     requires =  "Boost.Assert/1.64.0@bincrafters/testing", \
                       "Boost.Config/1.64.0@bincrafters/testing", \
                       "Boost.Core/1.64.0@bincrafters/testing", \
